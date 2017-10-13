3986505b7bd8099a9ebdbb53f62b796bed8375e0
Johnpaulho <johnpaulho@Johnpaulhos-MacBook-Pro.local>
2017-10-13 14:46:05 -0700

    Trying to find the real file

--- nameapp/helloViewController.swift
+++ nameapp/helloViewController.swift
@@ -1,5 +1,5 @@
 //
-//  helloViewController.swift
+//  HelloViewController.swift
 //  nameapp
 //
 //  Created by Johnpaulho on 10/10/17.
@@ -8,20 +8,22 @@
 
 import UIKit
 
-class helloViewController: UIViewController {
-    //missing ib outlet weak var label
+class HelloViewController: UIViewController {
+    @IBOutlet weak var greetingLabel: UILabel!
     var name = "Person"
     override func viewDidLoad() {
         super.viewDidLoad()
         greetingLabel.text = "Hello \(name)!"
         // Do any additional setup after loading the view.
     }
-
+    override func viewWillAppear(_ animated: Bool) {
+        greetingLabel.text = "Hello \(name)"
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    @IBAction func spendLastName(unwindSegue: UIStoryboardSegue){
+    @IBAction func appendLastName(unwindSegue: UIStoryboardSegue){
         
     }
     
